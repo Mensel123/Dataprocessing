891ed279f93949c5d3b99da16766ef6d3f58fd2a
Mendel Engelaer <mendelengelaer@wcw-stud-145-109-54-192.wireless.uva.nl>
2018-11-08 14:19:18 +0100

    data parsed

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,8 +1,26 @@
 
 import csv
-import pandas
+import pandas as pd
 
-with open('input.csv', newline='') as input_file:
-    data = csv.DictReader(input_file)
-    for row in data:
-        print(row["Country"])
+
+def load():
+    # load csv as DataFrame
+    df = pd.read_csv('input.csv')
+    return(df)
+
+
+def parsing(df):
+    # select columns of interest
+    df = df[['Country', 'Region', 'Pop. Density (per sq. mi.)',
+             'Infant mortality (per 1000 births)', 'GDP ($ per capita) dollars']]
+
+    # remove unknown and Nan data
+    df = df[df != 'unknown']
+    df = df.dropna()
+
+    print(df)
+
+
+if __name__ == "__main__":
+    df = load()
+    parsing(df)
