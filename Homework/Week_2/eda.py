9d5be2f17f9b678fb40f0e8ba9d0d8b49fbbba65
Mendel Engelaer <mendelengelaer@MBP-van-Mendel.uilenstede3.duwo.lan>
2018-11-11 16:59:17 +0100

    bezig met boxplot

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,11 +1,14 @@
 
 import csv
 import pandas as pd
+import re
+# import matplotlib.pyplot as plt
 
 
 def load():
     # load csv as DataFrame
     df = pd.read_csv('input.csv')
+
     return(df)
 
 
@@ -18,9 +21,40 @@ def parsing(df):
     df = df[df != 'unknown']
     df = df.dropna()
 
-    print(df)
+    return(df)
+
+
+def central_tendency(df):
+
+    # gdp = df["GDP ($ per capita) dollars"].tolist()
+    df = df["GDP ($ per capita) dollars"].str.extract('(\d+)', expand=False)
+    df = df.astype(int)
+    mean = df.mean()
+    median = df.median()
+    mode = df.mode()[0]
+    std = df.std()
+
+    # print(df)
+    # plt.hist(df,bins=50)
+    # plt.show()
+
+def five_num_tend(df):
+    df = df["Infant mortality (per 1000 births)"]
+    df = df.astype(float)
+
+    minimum = df.min()
+    maximum = df.max()
+    print(minimum)
+    # first_quartile = df.quantile(0.25)
+    # print(first_quartile)
+
+
+
+
 
 
 if __name__ == "__main__":
     df = load()
-    parsing(df)
+    parsed_df = parsing(df)
+    central_tendency(parsed_df)
+    five_num_tend(parsed_df)
