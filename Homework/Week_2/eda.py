458bca989f0f5734186c91a3cb37ee3c94e487a2
Mendel Engelaer <mendelengelaer@MBP-van-Mendel.uilenstede3.duwo.lan>
2018-11-12 20:22:10 +0100

    final changes

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,9 +1,6 @@
 
-import csv
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
-import json
 
 
 def load():
@@ -18,24 +15,36 @@ def parsing(df):
     df = df[['Country', 'Region', 'Pop. Density (per sq. mi.)',
              'Infant mortality (per 1000 births)', 'GDP ($ per capita) dollars']]
 
-    # remove unknown and Nan data
+    # remove 'unknown' and 'Nan' data
     df = df[df != 'unknown']
     df = df.dropna()
+
+    # remove outlier Suriname, a GDP of 400.000 is most probably a
+    # measurement error
+    df = df[df.Country != 'Suriname ']
+
+    # remove whitespace from Region string
+    for index, row in df.iterrows():
+        row["Region"] = row["Region"].rstrip()
 
     return(df)
 
 
 def central_tendency(df):
 
+    # extract numerical data to discart 'dollars'
     df = df["GDP ($ per capita) dollars"].str.extract('(\d+)', expand=False)
     df = df.astype(int)
+    # calculate mean, median, mode and standard error
     mean = df.mean()
     median = df.median()
     mode = df.mode()[0]
     std = df.std()
 
-    plt.hist(df,bins=50)
+    # plot histogram
+    # plt.hist(df)
     # plt.show()
+
 
 def five_num_tend(df):
     df = df["Infant mortality (per 1000 births)"]
@@ -43,9 +52,13 @@ def five_num_tend(df):
     # replace ',' with '.' so data can be saved as floats
     df = (df.str.split()).apply(lambda x: float(x[0].replace(',', '.')))
 
-    # print boxplot
-    plt.boxplot(df)
-    # plt.show()
+    # plot boxplot
+    plt.boxplot(df, patch_artist=True)
+    plt.title("Infant mortality of all countries")
+    plt.ylabel("Infant mortality (per 1000 births)")
+    plt.xticks([1], ['All countries'])
+    plt.show()
+
 
 def converting(df):
 
@@ -53,11 +66,7 @@ def converting(df):
     df = df.set_index('Country')
 
     # make json with Country as index
-    df_json = df.to_json(orient="index")
-
-    # open json file and write json objects into this file
-    json_file = open("country.json", 'w')
-    json.dump(df_json, json_file)
+    df.to_json('country.json', orient="index")
 
 
 if __name__ == "__main__":
