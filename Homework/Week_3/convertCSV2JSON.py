53559141feb70e10844e4caffe317747a5be57d6
Mendel Engelaer <mendelengelaer@wcw-stud-145-109-24-29.wireless.uva.nl>
2018-11-19 09:39:28 +0100

    json is formatted

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -5,29 +5,25 @@ import pandas as pd
 def load():
     # load csv as DataFrame
     df = pd.read_csv('fossil-fuel-co2-emissions-by-nation.csv')
-
     return(df)
 
 
 def parsing(df):
-    # select columns of interest
-    df = df[['Year', 'Country', 'Total']]
-
+    # select data from NETHERLANDS
     df = df.loc[lambda df: df.Country == 'NETHERLANDS', :]
 
-    # # remove 'unknown' and 'Nan' data
-    # df = df[df != 'unknown']
-    # df = df.dropna()
-
+    # select columns of interest
+    df = df[['Year', 'Total']]
     return(df)
 
 
 def converting(df):
+
     # set country as index
-    # df = df.set_index('Country')
+    df = df.set_index('Year')
 
     # make json with Country as index
-    df.to_json('country.json')
+    df.to_json('country.json', orient='index')
 
 
 if __name__ == "__main__":
